6e3f7025b63db239f38fe6270f762a6238df72e2
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-07-13 18:06:27 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
+import { Container } from './App.styled';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Notification from './Notification/Notification';
+import Section from './Section/Section';
+import { useState } from 'react';
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
+  };
+
+  const stateUpdate = option => {
+    if (option === 'good') {
+      setGood(good + 1);
+    } else if (option === 'neutral') {
+      setNeutral(neutral + 1);
+    } else {
+      setBad(bad + 1);
+    }
+  };
+
+  const options = ['good', 'neutral', 'bad'];
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Section title="Please leave feedback" />
+      <FeedbackOptions options={options} onBtnClick={stateUpdate} />
+      <Section title="Statistics" />
+      {countTotalFeedback() > 0 ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback}
+          positivePercentage={countPositiveFeedbackPercentage}
+        />
+      ) : (
+        <Notification message="No feedback given" />
+      )}
+    </Container>
   );
 };
+
+export default App;
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 50px auto;
+  width: 400px;
+  border: 3px solid #36426d;
+  border-radius: 8px;
+  box-shadow: 12px 12px 16px rgb(65, 64, 64), -12px -12px 16px rgb(91, 92, 94);
+  background-color: #c2c4ca;
+`;
