aca583fc788d848154c115ddb13ee216a9ea113a
Vladyslav-Starostyn <starostin.vlad.od@gmail.com>
2023-07-13 13:14:02 +0300

    ====

---
+++ src/components/Notification/Notification.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const NotificationMessage = styled.p`
+  margin-top: 0;
+  margin-bottom: 8px;
+  color: #353843;
+  margin-left: auto;
+  margin-right: auto;
+  font-weight: 600;
+  text-align: center;
+`;
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  margin-bottom: 20px;
+  font-size: 40px;
+  text-align: center;
+  color: #2B3457;
+`;
+export const SectionContainer = styled.div`
+  padding-top: 20px;
+`;
